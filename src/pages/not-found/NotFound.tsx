28f9bb0b1d3659b446c00d69085b8e2685ca4163
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-08-18 10:24:22 +0200

    feat: add style for 404

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,20 +1,15 @@
 import type { ReactNode } from 'react';
-import { useNavigate } from 'react-router-dom';
+
+import { Error } from '../components/404/Error';
+import { Footer } from '../components/footer/Footer';
+import { Header } from '../components/header/Header';
 
 export function NotFound(): ReactNode {
-  const navigate = useNavigate();
-
-  const handleClick = (): void => {
-    navigate('/');
-  };
-
   return (
-    <main className="main">
-      <h1>This is</h1>
-      <div>
-        <div>404</div>
-      </div>
-      <div onClick={handleClick}>Return on other side</div>
-    </main>
+    <>
+      <Header />
+      <Error />
+      <Footer />
+    </>
   );
 }
